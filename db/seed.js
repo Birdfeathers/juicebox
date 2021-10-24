c8a0a4e5bf5f665d50a6357ff5e9b899f8b5da74
Rebecca Carr <reca9238@colorado.edu>
2021-10-24 14:22:16 -0600

    completed part 3

--- db/seed.js
+++ db/seed.js
@@ -6,30 +6,63 @@ const {
     getAllPosts,
     createPost,
     updatePost,
-    getUserById
+    getUserById,
+   getPostsByTagName
   } = require('./index');
   
   async function createInitialPosts() {
     try {
       const [albert, sandra, glamgal] = await getAllUsers();
   
+      console.log("Starting to create posts...");
       await createPost({
         authorId: albert.id,
         title: "First Post",
-        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+        tags: ["#happy", "#youcandoanything"]
       });
   
-      // a couple more
+      await createPost({
+        authorId: sandra.id,
+        title: "How does this work?",
+        content: "Seriously, does this even do anything?",
+        tags: ["#happy", "#worst-day-ever"]
+      });
+  
+      await createPost({
+        authorId: glamgal.id,
+        title: "Living the Glam Life",
+        content: "Do you even? I swear that half of you are posing.",
+        tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+      });
+      console.log("Finished creating posts!");
     } catch (error) {
+      console.log("Error creating posts!");
       throw error;
     }
   }
-
+  
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+      await createInitialUsers();
+      await createInitialPosts();
+    } catch (error) {
+      console.log("Error during rebuildDB")
+      throw error;
+    }
+  }
+  
   async function dropTables() {
     try {
       console.log("Starting to drop tables...");
   
       await client.query(`
+        DROP TABLE IF EXISTS post_tags;
+        DROP TABLE IF EXISTS tags;
         DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
       `);
@@ -63,16 +96,17 @@ const {
           content TEXT NOT NULL,
           active BOOLEAN DEFAULT true
           );
+        CREATE TABLE tags (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL
+        );
+        CREATE TABLE post_tags (
+          "postId" INTEGER REFERENCES posts(id),
+          "tagId" INTEGER REFERENCES tags(id)
+        );
       `);
 
-      // await client.query(`
-      //     CREATE TABLE posts (
-      //       id SERIAL PRIMARY KEY,
-      //       "authorId" INTEGER REFERENCES users(id) NOT NULL,
-      //       title VARCHAR(255) NOT NULL,
-      //       content TEXT NOT NULL,
-      //       active BOOLEAN DEFAULT true
-      //     )
+      
       
   
       console.log("Finished building tables!");
@@ -91,6 +125,7 @@ const {
       await createInitialUsers();
       await createInitialPosts();
     } catch (error) {
+      console.log("Error during rebuildDB")
       throw error;
     }
   }
@@ -120,10 +155,20 @@ const {
         content: "Updated Content"
       });
       console.log("Result:", updatePostResult);
+
+      console.log("Calling updatePost on posts[1], only updating tags");
+      const updatePostTagsResult = await updatePost(posts[1].id, {
+        tags: ["#youcandoanything", "#redfish", "#bluefish"]
+      });
+      console.log("Result:", updatePostTagsResult);
   
       console.log("Calling getUserById with 1");
       const albert = await getUserById(1);
       console.log("Result:", albert);
+
+      console.log("Calling getPostsByTagName with #happy");
+      const postsWithHappy = await getPostsByTagName("#happy");
+      console.log("Result:", postsWithHappy);
   
       console.log("Finished database tests!");
     } catch (error) {
